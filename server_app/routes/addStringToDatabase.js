4fb2cb49dd1ea54b664f88b23aac8bc9de85ee8b
Mayank Wadhwani <mayankwadhwani@users-MacBook-Air.local>
2020-07-20 19:56:08 +0530

    intitial setup done

---
+++ server_app/routes/addStringToDatabase.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var router = express.Router();
+
+var doit = (req, res, next) => {
+    res.send("API is working successfully");
+
+};
+
+router.get("/", doit);
+
+module.exports = router;
